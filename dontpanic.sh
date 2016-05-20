ae135783cd5750e56698d07da2433b8be61bf83a
jannafierst <jlfierst@ua.edu>
2016-05-20 08:17:33 -0500

    Update dontpanic.sh

--- dontpanic.sh
+++ dontpanic.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Escaping the apostrophe can be tricky in awk
+
 awk 'BEGIN { print "Don\47t Panic!" }' # POSIX awk version
 
 awk 'BEGIN { print "Don\047t Panic!" }' # if followed by an octal digit, \047 is required
