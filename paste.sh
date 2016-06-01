143c7cc00a0b19867b604b1d9049487360182611
jannafierst <jlfierst@ua.edu>
2016-06-01 10:03:00 -0500

    Create paste.sh

---
+++ paste.sh
@@ -0,0 +1,14 @@
+# Some of the awesome things you can do with paste
+
+# Interleave two fastq files
+
+paste <(paste - - - - < reads-1.fastq) <(paste - - - - < reads-2.fastq) | tr '\t' '\n' > reads-int.fastq
+
+# Separate an interleaved fastq
+
+paste - - - - - - - - < reads-int.fastq | tee >(cut -f 1-4 | tr '\t' '\n' > reads-1.fastq) | cut -f 5-8 | tr '\t' '\n' > reads-2.fastq
+
+# Turn a fastq into a fasta
+
+paste - - - - < in.fastq | cut -f 1,2 | sed 's/^@/>/' | tr "\t" "\n" > out.fasta
+
