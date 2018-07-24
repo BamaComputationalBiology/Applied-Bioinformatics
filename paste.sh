0677d745a45eefb212fa2158b6230e23d72180dc
jannafierst <jlfierst@ua.edu>
2018-07-24 12:05:24 -0500

    Update paste.sh

--- paste.sh
+++ paste.sh
@@ -15,3 +15,7 @@ paste - - - - < in.fastq | cut -f 1,2 | sed 's/^@/>/' | tr "\t" "\n" > out.fasta
 # Select lines longer than X, here 2145 characters
 
 paste - - - - < in.fastq | awk 'length($0) > 2145' | tr "\t" "\n" > long.fastq
+
+# Remove duplicate entries in a .fastq
+
+paste - - - - < in.fastq | sort | uniq | tr "\t" "\n" > deduped.fastq
